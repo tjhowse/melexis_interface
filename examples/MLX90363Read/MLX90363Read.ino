878d78f4c87e19c37a55f97d50952f1c4be4b116
Berran Remzi <berranremzi@gmail.com>
2019-08-17 20:43:13 +0300

    Correct project name and header file

--- examples/MLX90363Read/MLX90363Read.ino
+++ examples/MLX90363Read/MLX90363Read.ino
@@ -1,31 +1,13 @@
 #include <SPI.h>
 
-/* Serial Monitor Example, Teensyduino Tutorial #3
-   http://www.pjrc.com/teensy/tutorial3.html
-
-   After uploading this to your board, use Serial Monitor
-   to view the message.  When Serial is selected from the
-   Tools > USB Type menu, the correct serial port must be
-   selected from the Tools > Serial Port AFTER Teensy is
-   running this code.  Teensy only becomes a serial device
-   while this code is running!  For non-Serial types,
-   the Serial port is emulated, so no port needs to be
-   selected.
-
-   This example code is in the public domain.
-*/
-
-#include "melexis.cpp"
+#include "melexis.h"
 
 MELEXIS test(15);
-
 
 void setup()
 {                
   Serial.begin(38400);  
 }
-
-
 
 void loop()
 {
